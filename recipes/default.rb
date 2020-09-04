6f71a4d418e76fa08ec0099a4091627c47de1df4
Jon DeCamp <jon.decamp@oracle.com>
2020-09-04 14:24:54 -0700

    setup google authenticator and update to clamav scanner

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,7 @@
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
 include_recipe '::base'
+include_recipe '::gauth'
 include_recipe '::firewalld'
 include_recipe '::clamav'
 include_recipe 'wazuh_agent::default'
