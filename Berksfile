f3c847d4fa23cb5369999d3d92b0044f526fa329
Jon DeCamp <jon.decamp@oracle.com>
2020-10-05 17:27:29 -0700

    alternate 2FA

--- Berksfile
+++ Berksfile
@@ -5,3 +5,5 @@ metadata
 cookbook 'firewalld', '~> 1.2.1'
 cookbook 'clamav', '~> 1.3.0'
 cookbook 'wazuh_agent', git: 'https://github.com/wazuh/wazuh-chef.git',rel: 'cookbooks/wazuh_agent'
+
+cookbook 'selinux_policy', '~> 2.4.3'
---
+++ recipes/totp.rb
@@ -0,0 +1,55 @@
+#
+# Cookbook:: oci_caas_bastion
+# Recipe:: totp
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+include_recipe 'yum-epel'
+
+package_list = %w(pam_oath oathtool gen-oath-safe)
+package_list.each do |pkg|
+  package "install #{pkg}" do
+    package_name pkg
+    action :install
+  end
+end
+
+directory '/home/opc/.oath' do
+  owner 'opc'
+  group 'opc'
+  mode '0700'
+end
+
+cookbook_file '/home/opc/.oath/setup-totp.sh' do
+  source 'setup-totp.sh'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+cookbook_file '/etc/pam.d/sshd' do
+  source 'pam.d.sshd'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  action :create
+end
+
+cookbook_file '/etc/ssh/sshd_config' do
+  source 'sshd_config'
+  owner 'root'
+  group 'root'
+  mode '0600'
+  action :create
+  notifies :restart, 'service[sshd]', :immediately
+end
+
+service 'sshd' do
+  action :enable
+end
+
+selinux_policy_fcontext '/home/opc/.oath(/.*)?' do
+  secontext 'ssh_home_t'
+  action :add
+end
