b1d544ff09456ebd101f0b2b8a8aba20f04488f0
Jon DeCamp <jjdecamp@dhcp-10-39-205-82.vpn.oracle.com>
2020-12-11 08:38:38 -0800

    move wazuh back to each app cookbook

--- Berksfile
+++ Berksfile
@@ -5,3 +5,4 @@ metadata
 cookbook 'firewalld', '~> 1.2.1'
 cookbook 'oci_caas_base', git: 'git@github.com:jondecamp/oci_caas_base.git', branch: 'main'
 cookbook 'selinux_policy', '~> 2.4.3'
+cookbook 'wazuh_agent', git: 'https://github.com/wazuh/wazuh-chef.git',rel: 'cookbooks/wazuh_agent'
--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,31 @@
-# Instead of using a json attributes file, you can override the defaults here:
-
+# runlist (cookbook) name for cron scheduling
 default['runlist_name'] = 'oci_caas_bastion'
+
+# Wazuh (ossec) settings
+override['ossec']['conf']['client']['server']['address'] = node['ossec']['registration_address']
+
+default['ossec']['conf']['syscheck']['ignore'] = [
+  '/etc/mtab',
+  '/etc/hosts.deny',
+  '/etc/mail/statistics',
+  '/etc/random-seed',
+  '/etc/random.seed',
+  '/etc/adjtime',
+  '/etc/httpd/logs',
+  '/etc/utmpx',
+  '/etc/wtmpx',
+  '/etc/cups/certs',
+  '/etc/dumpdates',
+  '/etc/svc/volatile',
+  '/sys/kernel/security',
+  '/sys/kernel/debug',
+  '/dev/core',
+  { '@type' => 'sregex', 'content!' => '^/proc' },
+  { '@type' => 'sregex', 'content!' => '.log$|.swp$' },
+]
+
+default['ossec']['conf']['syscheck']['directories'] = [
+  { '@check_all' => true, 'content!' => '/etc,/usr/bin,/usr/sbin' },
+  { '@check_all' => true, 'content!' => '/bin,/sbin,/boot' },
+  { '@check_all' => true, 'content!' => '/var/log/secure-*,/var/log/messages-*' },
+]
